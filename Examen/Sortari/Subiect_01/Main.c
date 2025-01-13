6815ac1660f52f67867b8c8354405af7bf9f60cd
Daria-1303 <daria.ciobanu@student.upt.ro>
2025-01-13 18:48:47 +0200

    Subiect 01 solved

--- Examen/Sortari/Subiect_01/Main.c
+++ Examen/Sortari/Subiect_01/Main.c
@@ -25,10 +25,12 @@ Este obligatorie exemplificarea efectului apelului funcțiilor implementate pe u
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 typedef struct{
 	int *data;
-	int n;
+	int size;
 }Tablou_t;
 
 // cea mai eficienta metoda de sortare ar fi QuickSort, deoarece are complexitatea O(n log n) in cazul mediu si O(n^2) in cazul
@@ -39,26 +41,117 @@ typedef struct{
 // si in cazul mediu, dar MergeSort are nevoie de un vector auxiliar pentru a sorta elementele, ceea ce ar face algoritmul mai ineficient
 // decat QuickSort.
 
-void swap(Tablou_t* el1, Tablou_t* el2)
+void swap(int* el1, int* el2)
 {
-	Tablou_t tmp;
+	int tmp;
 	tmp = *el1;
 	*el1 = *el2;
 	*el2 = tmp;
 }
 
-void quicksort(Tablou_t tablou[], int primElement, int ultimElement){
+void quicksort(Tablou_t tablou, int primElement, int ultimElement) { // O(n * log n) in cazul mediu si O(n^2) in cazul cel mai defavorabil
 	int stanga = primElement + 1;
 	int dreapta = ultimElement;
 
 	// alegere pivot
-	swap(&a[primElement], &a[(primElement + ultimElement) / 2]);
+	swap(&tablou.data[primElement], &tablou.data[(primElement + ultimElement) / 2]);
+
+	//mutare pivot pe prima pozitie 
+	int pivot = tablou.data[primElement];
+
+	// ne ocupam de partitionare
+
+	while (stanga <= dreapta) {	
+
+		while (stanga <= ultimElement && tablou.data[stanga] < pivot) {  //ordoneaza crescator,
+			stanga++;
+		}
+
+		while (dreapta >= primElement && pivot < tablou.data[dreapta]) { // ordoneaza crescator
+			dreapta--;
+		}
+
+		if (stanga < dreapta) {
+			swap(&tablou.data[stanga++], &tablou.data[dreapta--]);
+		}
+		else {
+			stanga++;
+		}
+	}
+
+	//mutam pivot la locul lui sau la final
+	swap(&tablou.data[dreapta], &tablou.data[primElement]);
+
+	//apeluri recursive
+	if (primElement < dreapta - 1) {
+		quicksort(tablou, primElement, dreapta - 1); 
+	}
+
+	if (dreapta + 1 < ultimElement) {
+		quicksort(tablou, dreapta + 1, ultimElement);
+	}
+}
+
+int separareValoriNegativeDePozitive(Tablou_t tablou) {
+	int index = 0;
+
+	// avem o functie de sortare valori negative(in prima parte a vectorului
+	// si valori pozitive(in a doua parte a vectorului) si returnam indexul de unde incepe partea a doua a vectorului
+	// pentru a apela quicksort pe acea parte a vectorului
+	// O(n) -> n - dimensiunea tabloului
+	for (int i = 0; i < tablou.size; i++) { 
+		if (tablou.data[i] < 0) {
+			if (i != index) {
+				swap(&tablou.data[i], &tablou.data[index]);
+			}
+			index++;
+		}
+	}
+
+	return index;
+}
+
+void printTablou(Tablou_t tablou) {
+	for (int i = 0; i < tablou.size; i++) {
+		printf("%d ", tablou.data[i]);
+	}
+	printf("\n");
 }
 
 
-
-
 int main(void) {
-	printf("da\n");
+	srand(time(NULL));
+
+	Tablou_t tablou;
+	
+	tablou.size = rand() % 20; // 6 elemente
+
+	tablou.data = (int*)malloc(tablou.size * sizeof(int));
+
+	for (int i = 0; i < tablou.size; i++) {
+		tablou.data[i] = rand() % 100 - 50;
+	}
+
+	printf("Tabloul initial: \n");
+	printTablou(tablou);
+
+	printf("--------------------\n");
+
+	int index = separareValoriNegativeDePozitive(tablou);
+
+	printf("Tabloul dupa separare: \n");
+	printTablou(tablou);
+
+	printf("--------------------\n");
+
+	printf("Tabloul dupa sortare: \n");
+	quicksort(tablou, index, tablou.size - 1);
+
+	printTablou(tablou);
+
+	printf("--------------------\n");
+
+	free(tablou.data);
+
 	return 0;
 }
---
+++ Examen/Sortari/Subiect_02/Main.c
@@ -0,0 +1,20 @@
+﻿/*
+Se consideră un tablou de structuri de forma:
+	            struct agenda {
+                    char nume[25];
+                    char telefon[10];
+                    int an;
+                    int venit;
+                };
+
+Fiind dat tabloul cu dimensiunea N, se cere implementarea a câte o funcție pentru:
+
+a) Mutarea pe ultimele poziții din vector a primelor două persoane cu un venit mai mic de 1000.
+
+b) Sortarea eficientă, în ordine alfabetică a numelor, a persoanelor cu vârsta între 30 și 40 de ani.
+
+Notă: Se consideră un exemplu de tablou cu minimum 5 astfel de structuri și se va exemplifica cum va arăta tabloul după 
+
+apelul fiecărei funcții.
+
+*/
