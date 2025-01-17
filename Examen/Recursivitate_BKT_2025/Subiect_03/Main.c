82a7dd6f0e9d353d80ea2e570b4a9803ead52e96
Daria-1303 <daria.ciobanu@student.upt.ro>
2025-01-17 21:58:59 +0200

    flood innebunesc 🐜

---
+++ Examen/Recursivitate_BKT_2025/Subiect_03/Main.c
@@ -0,0 +1,36 @@
+﻿/*
+	Enunț:
+
+	Un teren este reprezentat sub forma unei matrice A cu m linii și n coloane. 
+	
+	Elementele A[i][j] aparțin mulțimii numerelor reale și reprezintă cotele (înălțimea) diferitelor 
+	
+	porțiuni din acest teren. Se presupune că:
+
+		O bilă se găsește pe o porțiune A[a][b] (citită de la tastatură).
+
+		Bila se poate deplasa pe orice porțiune vecină (orizontal, vertical sau diagonal), 
+		
+		doar dacă cota porțiunii vecine este strict inferioară cotei curente.
+
+
+	Cerințe:
+
+	Să se precizeze toate traseele pe care bila le poate urma de la poziția de start, până când aceasta va ieși din teren.
+
+	Să se implementeze funcționalitatea algoritmului și structurile de date necesare.
+
+	Să se exemplifice funcționarea algoritmului pentru o matrice de dimensiune minimă 3×3.
+
+
+	Notă:
+
+	Algoritmul poate fi scris în pseudocod sau în C.
+
+	Este obligatorie comentarea algoritmului și justificarea complexității acestuia în termeni de O(f(n)).
+
+	Trebuie justificată alegerea structurilor de date utilizate.
+
+	Se va exemplifica efectul funcționalităților implementate pe un exemplu ales.
+
+*/
