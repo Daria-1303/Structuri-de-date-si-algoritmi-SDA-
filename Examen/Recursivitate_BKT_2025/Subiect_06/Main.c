f7413a5698baf1dcb01e6ba2e6c9c46e5db84b90
Daria-1303 <daria.ciobanu@student.upt.ro>
2025-01-18 18:51:17 +0200

    nr binar bkt 👍

--- Examen/Recursivitate_BKT_2025/Subiect_06/Main.c
+++ Examen/Recursivitate_BKT_2025/Subiect_06/Main.c
@@ -20,3 +20,84 @@ Suplimentar:
 	Să se scrie cel puțin trei dintre soluțiile problemei.
 
 */
+
+#include <stdio.h>
+
+#define MAX_CIFRE 6
+
+int numar[MAX_CIFRE];
+int pos[] = { 0, 1 };
+
+int acceptabil(int pos, int k) {
+	int nr_zero = 0;
+	int nr_unu = 0;
+
+	for (int i = 0; i < k; i++) {
+		if (numar[i] == 0) {
+			nr_zero++;
+		}
+		if (numar[i] == 1) {
+			nr_unu++;
+		}
+		if (i == 1 && nr_unu == 2) {
+			return 0;
+		}
+	}
+
+	if (pos == 0 && k == (MAX_CIFRE - 1)) {
+		return 0;
+	}
+
+	if (pos == 0 && nr_zero < 2) {
+		return 1;
+	}
+
+	if (pos == 1 && nr_unu < 4) {
+		return 1;
+	}
+
+
+	return 0;
+}
+
+
+int eSolutie(int k) {
+	return k == MAX_CIFRE;
+}
+
+int solutiiAfisate = 0;
+
+void afiseazaSolutie() {
+	for (int i = 0; i < MAX_CIFRE; i++) {
+		printf("%d", numar[i]);
+	}
+
+	printf("\n");
+
+	solutiiAfisate++;
+}
+
+void permuta(int k) {
+	int i;
+	int aux;
+
+	if (eSolutie(k)) {
+		afiseazaSolutie();
+	}
+	else {
+		for (int i = 0; i < 2; i++) {
+			aux = pos[i];
+			if (acceptabil(aux, k)) {
+				numar[k] = pos[i];
+				permuta(k + 1);
+			}
+		}
+	}
+}
+
+int main(void) {
+	permuta(0);
+
+	printf("\n Solutii: %d", solutiiAfisate);
+	return 0;
+}
