b1ef418344cc7e9f16c07a977896e5445bd2c108
Daria-1303 <daria.ciobanu@student.upt.ro>
2025-01-19 18:57:59 +0200

    evenimente ti

---
+++ Examen/Subiect_TI_2024/Multilista_coada_prioritate/Main.c
@@ -0,0 +1,20 @@
+﻿/*
+			Prima problemă
+	Se cere implementarea unei structuri de date care să permită gestionarea unor evenimente, 
+	
+	fiecare fiind caracterizat de un ID, un moment de activare și o prioritate. Este necesar să:
+
+			Se sorteze evenimentele după prioritate și moment de activare.
+
+			Se utilizeze o coadă FIFO pentru a gestiona evenimentele cu prioritate egală.
+
+			Să se afișeze rezultatele gestionării evenimentelor.
+
+			Algoritmul trebuie să aibă complexitatea O(nlogn).
+	
+
+			A doua problemă
+
+	Se cere implementarea unei funcții recursive pentru a parcurge și afișa o structură de date aleasă anterior.
+
+*/
