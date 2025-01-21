6ba364524edfbdc8af8041174e7fccf0729c099a
Daria-1303 <daria.ciobanu@student.upt.ro>
2025-01-21 18:51:27 +0200

    subiecte examene p1

--- Examen/Sortari 2025/Subiect_02/Main.c
+++ Examen/Sortari 2025/Subiect_02/Main.c
@@ -87,29 +87,101 @@ void swap(Agenda_t* a, Agenda_t* b) {
 	precum QuickSort, MergeSort, dar pentru dimensiuni mici, Selection sort este suficient de eficient
 */
 
-void cerintaB_sortareNume(Agenda_t* agende) {
-	int i, j, min;
+//void cerintaB_sortareNume(Agenda_t* agende) {
+//	int i, j, min;
+//
+//	for (i = 0; i < N - 1; i++) {
+//		if (agende[i].an >= 30 && agende[i].an <= 40) {
+//			min = i;
+//		}
+//		else {
+//			continue;
+//		}
+//
+//		for (j = i + 1; j < N; j++) {
+//			if (agende[j].an >= 30 && agende[j].an <= 40) {
+//				if (strcmp(agende[j].nume, agende[min].nume) < 0) {
+//					min = j;
+//				}
+//			}
+//		}
+//
+//		swap(&agende[i], &agende[min]);
+//	}
+//}
 
-	for (i = 0; i < N - 1; i++) {
-		if (agende[i].an >= 30 && agende[i].an <= 40) {
-			min = i;
-		}
-		else {
-			continue;
-		}
+/*
+void mergesort(tip_element A[], tip_element temp[], int left, int right) {
+	if (left == right) return; // secventa de un element
 
-		for (j = i + 1; j < N; j++) {
-			if (agende[j].an >= 30 && agende[j].an <= 40) {
-				if (strcmp(agende[j].nume, agende[min].nume) < 0) {
-					min = j;
-				}
-			}
-		}
+	int mid = (left + right) / 2;
 
-		swap(&agende[i], &agende[min]);
+	mergesort(A, temp, left, mid);
+	mergesort(A, temp, mid + 1, right);
+
+	for (int i = left; i <= right; i++) temp[i] = A[i]; // se copiaza secventa in temp
+
+	int i1 = left; int i2 = mid + 1; // interclasare inapoi in A
+
+	for (int index = left; index <= right; index++) {
+		if (i1 == mid + 1) A[index] = temp[i2++];  // s-a epuizat secventa din stanga
+		else if (i2 > right) A[index] = temp[i1++]; // s-a epuizat secventa din dreapta
+		else if (temp[i1].cheie < temp[i2].cheie) A[index] = temp[i1++];
+		else A[index] = temp[i2++];
 	}
 }
+*/
 
+void quicksort(Agenda_t a[], int prim, int ultim)
+{
+	int stanga = prim + 1;
+	int dreapta = ultim;
+	//alegere pivot
+	swap(&a[prim], &a[(prim + ultim) / 2]);
+	//mutare pivot pe prima pozitie
+	Agenda_t pivot = a[prim];
+	while (stanga <= dreapta) //partitionare
+	{
+		while (stanga <= ultim && (strcmp(a[stanga].nume, pivot.nume) < 0))
+			stanga++;
+		while (dreapta >= prim && (strcmp(a[dreapta].nume, pivot.nume) > 0))
+			dreapta--;
+		if (stanga < dreapta)
+			swap(&a[stanga++], &a[dreapta--]);
+		else
+			stanga++;
+	}
+	//mutare pivot la locul sau final
+	swap(&a[dreapta], &a[prim]);
+	//apelurile recursive
+	if (prim < dreapta - 1)
+		quicksort(a, prim, dreapta - 1);
+	if (dreapta + 1 < ultim)
+		quicksort(a, dreapta + 1, ultim);
+}
+
+void sortare(Agenda_t a[]) {
+	int newSize = 0;
+	Agenda_t eligibili[N];
+	int indecsi[N];
+
+	//O(N) pentru selectarea persoanelor eligibile
+	for (int i = 0; i < N; i++) {
+		if (a[i].an >= 30 && a[i].an <= 40) {
+			eligibili[newSize] = a[i];
+			indecsi[newSize] = i;
+			newSize++;
+		}
+	}
+
+	//O(n * logn) pentru sortarea persoanelor eligibile
+
+	quicksort(eligibili, 0, newSize - 1);
+
+	for (int i = 0; i < newSize; i++) {
+		a[indecsi[i]] = eligibili[i];
+	}
+}
 
 int main(int argc, char *argv[]) {
 	if (argc != 2) {
@@ -136,7 +208,8 @@ int main(int argc, char *argv[]) {
 	afiseazaDateAgende(agende);
 
 	printf("Cerinta B : \n");
-	cerintaB_sortareNume(agende);
+	//cerintaB_sortareNume(agende);
+	sortare(agende);
 	afiseazaDateAgende(agende);
 
 	return 0;
